7d9c48355e315ec5517f6de4d1217d8a45be90b2
deshayk <deshaykidd@gmail.com>
2022-06-07 00:49:30 -0500

    add drawing function to sketch

--- project-sample/sketch.js
+++ project-sample/sketch.js
@@ -1,7 +1,19 @@
-function setup() {
-  createCanvas(1000, 500);
+function setup() { //setup canvas and create a new p5.js sketch
+  var cnv = createCanvas(windowWidth, windowHeight);
+  cnv.style('display', 'block');
+}
+
+function windowResized() { //resizes canvas to window size
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  // background(220);
+  // function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
